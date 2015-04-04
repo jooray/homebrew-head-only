6e6055f540e159afc54a9f3ec9d7f0151583c646
Juraj Bednar <juraj@bednar.sk>
2015-04-04 05:02:35 +0200

    Add ionice

--- ionice.rb
+++ ionice.rb
@@ -1,14 +1,11 @@
-require 'formula'
-
 class Ionice < Formula
-  homepage 'https://github.com/elmarb/ionice'
-  url 'https://github.com/elmarb/ionice.git', :revision => '63539195ba6aa082376a53b09a47b33c25a9b204'
-  sha1 '03effb6c344a04c9fea2f995ceb4fc8fa270d310'
-  version '20120202'
+  homepage "https://github.com/elmarb/ionice"
+  url "https://github.com/elmarb/ionice.git", :revision => "63539195ba6aa082376a53b09a47b33c25a9b204"
+  version "20120202"
 
   def install
     system "#{ENV.cc} #{ENV.cflags} -o ionice ionice.c"
-    bin.install 'ionice'
+    bin.install "ionice"
   end
 
   test do
